f567a5a7ada45d36d0347c93a63757a1c5ea066f
Johannes <johannes-pahle@t-online.de>
2022-07-29 13:16:54 +0200

    :sparkles: basic login system

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import List from "./components/List";
 import Timer from "./components/Timer";
@@ -9,15 +9,60 @@ import roadtrip from "./roadtrip.svg";
 import Plans from "./components/Plans";
 
 function App() {
+  const [showInput, setShowInput] = useState(false);
+
+  const [user, setUser] = useState(Number(localStorage.getItem("userlevel")));
+
+  const validate = (e: string) => {
+    //! CRITICAL!
+    switch (e) {
+      case "5555":
+        console.log("Authorized Nr. 1");
+        setUser(1);
+        localStorage.setItem("userlevel", "1");
+        break;
+      case "8451":
+        console.log("Authorized Nr. 2");
+        setUser(2);
+        localStorage.setItem("userlevel", "2");
+        break;
+      case "0605":
+        console.log("Authorized Nr. 3");
+        setUser(3);
+        localStorage.setItem("userlevel", "3");
+        break;
+      case "0000":
+        console.log("Logged out!");
+        setUser(0);
+        localStorage.setItem("userlevel", "0");
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="App">
       <div className="roadtrip-img">
-        <img src={roadtrip} />
+        <img src={roadtrip} onDoubleClick={() => setShowInput(!showInput)} />
       </div>
+      {showInput && (
+        <div className="login">
+          <input
+            type={"tel"}
+            autoFocus={true}
+            maxLength={4}
+            placeholder="Passwort"
+            onChange={(e) => validate(e.target.value)}
+          />
+          <p>Willkommen lvl. {user}</p>
+        </div>
+      )}
       <Timer />
-      <Cost />
+      {user >= 2 && <Cost />}
       <Plans />
-      <List />
+      {user >= 2 && <List />}
       <Map />
       <div className="footer" />
     </div>
